cac0fca27974836c5ce417ebb23c4885de1d5d61
wrpaiva-labs <wrpaiva2607@gmail.com>
2024-10-09 19:49:25 -0300

    Mudando o texto do rest

--- src/main/java/br/com/paiva/hexagonal/adapter/in/api/GreetingResource.java
+++ src/main/java/br/com/paiva/hexagonal/adapter/in/api/GreetingResource.java
@@ -12,7 +12,7 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from Quarkus REST";
+        return "Quarkus Hexagonal REST";
     }
 
 }
