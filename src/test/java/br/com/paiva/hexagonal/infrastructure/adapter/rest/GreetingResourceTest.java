f56e797243a12a17c6a5427136e95c93390f11ea
wrpaiva-labs <wrpaiva2607@gmail.com>
2024-10-09 20:00:20 -0300

    Ajustando test com a mensagem correta

--- src/test/java/br/com/paiva/hexagonal/infrastructure/adapter/rest/GreetingResourceTest.java
+++ src/test/java/br/com/paiva/hexagonal/infrastructure/adapter/rest/GreetingResourceTest.java
@@ -15,6 +15,6 @@ class GreetingResourceTest {
           .when().get("/greetings")
           .then()
              .statusCode(200)
-             .body(is("Hello from Quarkus REST"));
+             .body(is("Quarkus Hexagonal REST"));
     }
 }
